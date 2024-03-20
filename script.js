631377d622022d2e98792408f1939e36d98aef39
quin <queenshafaraniabilqis@gmail.com>
2024-03-20 13:51:46 +0700

    completed it. Taks: Photo and Animation

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  string: ["Frontend Developer", "Website Developer", "Student"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
